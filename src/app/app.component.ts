67c3b8add7d69363e648733a397eb5b96c340faa
Rubén <aparicio.ruben@gmail.com>
2021-05-17 18:49:10 +0200

    Added check for update and reload app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import * as dayjs from 'dayjs';
 import 'dayjs/locale/es';
 
@@ -11,8 +12,24 @@ export class AppComponent implements OnInit{
 
   title = 'Scenio Tools';
 
+  constructor(private swUpdate: SwUpdate){
+    this.checkUpdate();
+  }
 
   ngOnInit(): void {
     dayjs.locale('es');
   }
+
+  checkUpdate() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('Actualización disponible. Se va a recargar la página.')) {
+          this.swUpdate.activateUpdate().then(() => document.location.reload());
+        } else {
+          this.swUpdate.activateUpdate().then(() => document.location.reload());
+        }
+      });
+      this.swUpdate.checkForUpdate();
+    }
+}
 }
